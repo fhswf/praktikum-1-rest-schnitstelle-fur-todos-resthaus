943cdf80387dde37b4452972ebbcf248bc4c023b
Dominik Bock <57663621+DoBo91@users.noreply.github.com>
2023-03-24 16:10:28 +0000

    Anpassungen für den Praktikumstag 1

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
-import express from 'express';
+import express, { response } from 'express';
 
 /** Zentrales Objekt für unsere Express-Applikation */
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 /**
  * Liste aller ToDos. 
@@ -22,4 +24,76 @@ let TODOS = [
     },
 ];
 
-// Your code here
+
+app.get('/', (request, response) => response.redirect('/todos'));
+
+
+app.get('/todos', (req, res) => {
+    res.send(TODOS)
+  })
+
+ 
+app.get('/todos/:id', (req, res) => {  
+    const id = parseInt(req.params['id'])  
+    for (let i=0; i<TODOS.length; i++) { 
+        if (TODOS[i].id === id){
+            res.send(TODOS[i])
+            break;         
+        }
+        }       
+    res.send(id + " ist nicht vorhanden")    
+})
+
+
+app.post('/todos', (req, res) => {
+    const input = req.body;
+    if (!input.id) {
+        res.status(400).send('Todo muss eine ID haben');
+        return;
+    }
+    for (let i=0; i<TODOS.length; i++) { 
+        if (TODOS[i].id === input.id){
+            res.status(409).send("Anlegen nicht möglich. ID schon vorhanden");
+            return;
+        }
+    }
+    TODOS.push(input);
+    res.send(TODOS);
+});
+
+app.put("/todos/:id", (req, res) => {
+    const id = parseInt(req.params['id'])
+    const input = req.body;
+    for (let i=0; i<TODOS.length; i++) { 
+        if (TODOS[i].id === id){
+            TODOS[i].title = input.title;
+            TODOS[i].due = input.due;
+            TODOS[i].status = input.status;
+            res.send(id + " wurde erfolgreich geändert") 
+        }
+        }       
+    res.send(id + " ist nicht vorhanden") 
+});
+
+app.delete("/todos/:id", (req, res) => {
+    const id = parseInt(req.params['id'])      
+    for (let i=0; i<TODOS.length; i++) { 
+        if (TODOS[i].id === id){
+            TODOS.splice(i, 1);
+            res.send(id + " wurde erfolgreich gelöscht")        
+        }
+        }       
+    res.send(id + " ist nicht vorhanden") 
+})
+
+
+
+
+
+app.listen(1337, () => {
+    console.log('Server is listening to https://dobo91-automatic-chainsaw-q44gw6rgw96c67-1337.preview.app.github.dev/');
+}); 
+
+app.listen(1234, () => {
+    console.log('Server is listening to https://dobo91-automatic-chainsaw-q44gw6rgw96c67-1337.preview.app.github.dev/');
+}); 
